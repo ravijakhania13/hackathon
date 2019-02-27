2d3fd35e8f262e0613c222e402edc6e36902f5f2
Pranjali Ingole <pranjali.ingole@students.iiit.ac.in>
2019-02-28 00:17:29 +0530

    updated client.py

--- Hackthon/Client.py
+++ Hackthon/Client.py
@@ -12,12 +12,18 @@ df = pd.read_csv('Sonar.csv')
 eval_data = df[df.columns[0:60]].values
 eval_labels = df[df.columns[60]]
 
-data = json.dumps({"signature_name": "model", "instances": eval_data[0].tolist()})
+Predictions_list = []
+# for i in range(eval_data.shape[0]):
+
+data = json.dumps({"signature_name": "model", "instances": eval_data.tolist()})
+
 
 headers = {"content-type": "application/json"}
-json_response = requests.post('http://10.2.133.28:8501/v1/models/sonar_model:predict', data=data, headers=headers)
+json_response = requests.post('http://10.2.133.230:8501/v1/models/sonar_model:predict', data=data, headers=headers)
 print (json_response.text)
 
-predictions = json.loads(json_response.text)
+predictions = json.loads(json_response.text)["predictions"]
 
-print ("Prediction: ", np.argmax(predictions[0]))
+predictions = np.array(predictions)
+
+print ("Prediction: ", np.argmax(predictions, axis=1))
